0188d3b588b11eca721775c9b28b6b0c959cb859
paperbenni <paperbenni@gmail.com>
2023-09-12 14:54:58 +0200

    init 2

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
              '("melpa" . "https://melpa.org/packages/"))
 
 (defvar my-packages
-  '(catppuccin-theme evil)
+  '(catppuccin-theme evil nov calibre pdf-tools)
   "A list of packages to be installed at launch"
   )
 
@@ -25,8 +25,14 @@
 
 (require 'evil)
 (evil-mode 1)
+(recentf-mode 1)
 (hl-line-mode 1)
+(savehist-mode 1)
 
+(pdf-loader-install)
+(add-hook 'pdf-view-mode-hook #'pdf-links-minor-mode)
+
+(add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
 
 (setq display-line-numbers 'relative)
 
@@ -38,10 +44,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(calibre-libraries '(("books" . "~/Books")))
  '(custom-enabled-themes '(catppuccin))
  '(custom-safe-themes
    '("26149a1b5de476aa661bbb9c8f79540509c038fbba58c1c719466851c2968464" default))
- '(package-selected-packages '(evil)))
+ '(package-selected-packages '()))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
